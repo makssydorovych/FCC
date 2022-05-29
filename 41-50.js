10c1115d0e1c2b7b142ebd5cee0913e73bbd4523
MaksSydorovych <makssydorovych@gmail.com>
2022-05-29 18:35:31 +0200

    Update 41-50.js

--- 41-50.js
+++ 41-50.js
@@ -17,3 +17,11 @@ const myArray = [
 ];
 
 const myData = myArray[2][1];
+//Push ["dog", 3] onto the end of the myArray variable.
+// Setup
+const myArray = [
+	["John", 23],
+	["cat", 2],
+];
+
+myArray.push(["dog", 3]);
