c4c14089ca40716a9bb5b561340c32927d2c4658
MaksSydorovych <makssydorovych@gmail.com>
2022-06-03 22:44:17 +0200

    82

---
+++ 81-90/82.js
@@ -0,0 +1,4 @@
+// Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+// Hint
+// Remember that undefined is a keyword, not a string.
