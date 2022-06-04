11d8f651b2c17a1efe5b555e0ecb56c2c63d3a1c
MaksSydorovych <makssydorovych@gmail.com>
2022-06-04 09:16:22 +0200

    83

--- 81-90/82.js
+++ 81-90/82.js
@@ -2,3 +2,13 @@
 
 // Hint
 // Remember that undefined is a keyword, not a string.
+function abTest(a, b) {
+	// Only change code below this line
+	if (a < 0 || b < 0) {
+		return undefined;
+	} else {
+	}
+	// Only change code above this line
+
+	return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
